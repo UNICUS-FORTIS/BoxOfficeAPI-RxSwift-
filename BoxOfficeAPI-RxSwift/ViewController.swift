9f595a3e00c6715eae1b904233c2f1262ba0606f
Louie Shin <unicus.fortis@gmail.com>
2023-11-06 20:36:05 +0900

    Initial Commit

---
+++ BoxOfficeAPI-RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoxOfficeAPI-RxSwift
+//
+//  Created by LOUIE MAC on 11/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
